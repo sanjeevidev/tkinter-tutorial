5bae223c16658b6cadf22700576a114cdf3719fb
Sanjeevi Kumar V <sanjeevikumar116@gmail.com>
2023-08-01 22:24:39 +0530

    🚀

---
+++ Day-1-window.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter.ttk import *
+
+# main window object named root
+root = Tk()
+
+# giving title to the main window
+root.title("First_Program")
+
+# Label is what output will be
+label = Label(root, text ="Hello World !").pack()
+root.mainloop()
