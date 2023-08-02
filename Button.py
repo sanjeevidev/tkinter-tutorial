16e8acbb17afa4fb7739655cfcd48626b6d07632
Sanjeevi Kumar V <sanjeevikumar116@gmail.com>
2023-08-02 16:39:14 +0530

    🚀

--- Button.py
+++ Button.py
@@ -1,4 +1,4 @@
-#Start and Stop Buttons
+#Example-1 - Start and Stop Buttons
 import tkinter as tk
 from tkinter import messagebox
 
@@ -9,8 +9,20 @@ def show():
 
 button = tk.Button(r, text='Stop', width=25, command=r.destroy)
 button1 = tk.Button(r, text='Start', width=20, command=show)
-
 button.pack()
 button1.pack()
 
 r.mainloop()
+
+#Example-2 - Button with Different Colors
+import tkinter as tk
+
+def on_button_click():
+    print("Button clicked!")
+
+root = tk.Tk()
+root.title("Colored Button Example")
+button = tk.Button(root, text="Click Me!", command=on_button_click, bg="black", fg="white")
+button.pack()
+
+root.mainloop()
