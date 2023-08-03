f2f6f7ae67101c8f16d83a583cc3009b0086fa3b
Sanjeevi Kumar V <sanjeevikumar116@gmail.com>
2023-08-03 16:26:37 +0530

    🚀

---
+++ Widgets/button.py
@@ -0,0 +1,21 @@
+# <<< EXAMPLE-1 >>>
+from tkinter import *
+
+root = Tk()
+root.geometry('100x100')
+btn = Button(root, text = 'Click me !', bd = '7', command = root.destroy)
+btn.pack(side = 'top')
+
+root.mainloop()
+
+# <<< EXAMPLE-2 >>>
+# Following will import tkinter.ttk module and automatically override all the widgets which are present in tkinter module.
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()		
+root.geometry('100x100')	
+btn = Button(root, text = 'Click me !', command = root.destroy)
+btn.pack(side = 'top')	
+
+root.mainloop()
